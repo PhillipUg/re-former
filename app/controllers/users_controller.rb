c7357c5b6c6d40a3f188201ce1588f99128aa0ac
phillipug <phillipmusiime@gmail.com>
2020-05-28 17:58:17 +0300

    rubocop corrections

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,27 @@
 class UsersController < ApplicationController
   def new
-  	@user = User.new
+    @user = User.new
   end
   
   def edit
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def create
-  	# @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-  	@user = User.new(user_params)
-  	if @user.save
-  		redirect_to new_user_path
-  	else
-  		render :new
-  	end
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
-
 
   def update
     @user = User.find(params[:id])
     
     if @user.update(user_params)
-      render plain: "User updated Succefully"
+      render plain: 'User updated Succefully'
     else
       render :edit
     end
@@ -31,6 +30,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-  	params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
